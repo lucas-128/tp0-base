9876ebca8fbbe376e3e6010e78a35ee9b1af1401
Lucas <lgrati@fi.uba.ar>
2024-08-26 16:48:08 -0300

    ejercicio 1

---
+++ generar-compose.sh
@@ -0,0 +1,10 @@
+if [ "$#" -ne 2 ]; then
+    echo "Uso: $0 <output_file_name> <number_of_clients>"
+    exit 1
+fi
+
+output_file=$1
+num_clients=$2
+
+python3 mi-generador.py "$output_file" "$num_clients"
+echo "Docker Compose file '$output_file' generado con $num_clients clientes."
---
+++ mi-generador.py
@@ -0,0 +1,54 @@
+import sys
+
+def generate_docker_compose(output_file, num_clients):
+    compose_content = """version: '3.8' 
+
+services:
+  server:
+    container_name: server
+    image: server:latest
+    entrypoint: python3 /main.py
+    environment:
+      - PYTHONUNBUFFERED=1
+      - LOGGING_LEVEL=DEBUG
+    networks:
+      - testing_net
+"""
+
+    for i in range(1, num_clients + 1):
+        client_name = f"client{i}"
+        client_service = f"""  {client_name}:
+    container_name: {client_name}
+    image: client:latest
+    entrypoint: /client
+    environment:
+      - CLI_ID={i}
+      - CLI_LOG_LEVEL=DEBUG
+    networks:
+      - testing_net
+    depends_on:
+      - server
+"""
+        compose_content += client_service
+
+    compose_content += """
+networks:
+  testing_net:
+    ipam:
+      driver: default
+      config:
+        - subnet: 172.25.125.0/24
+"""
+
+    with open(output_file, 'w') as file:
+        file.write(compose_content)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Uso: python3 mi-generador.py <output_file_name> <number_of_clients>")
+        sys.exit(1)
+
+    output_file = sys.argv[1]
+    num_clients = int(sys.argv[2])
+
+    generate_docker_compose(output_file, num_clients)
