706ca98555ffe9efd5e9cfe1920ea665db6e0033
Lucas <lgrati@fi.uba.ar>
2024-08-29 17:27:53 -0300

    ejercicio 3

---
+++ validar-echo-server.sh
@@ -0,0 +1,13 @@
+NETWORK_NAME="tp0-base_testing_net"
+SERVICE_NAME="server"  
+PORT="12345"
+MESSAGE="Test"
+
+docker run --rm --network "$NETWORK_NAME" busybox sh -c "
+  RESPONSE=\$(echo '$MESSAGE' | nc $SERVICE_NAME $PORT)
+  if [ \"\$RESPONSE\" = \"$MESSAGE\" ]; then
+    echo 'action: test_echo_server | result: success'
+  else
+    echo 'action: test_echo_server | result: fail'
+  fi
+"
