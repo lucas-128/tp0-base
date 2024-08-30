dceb4f27a87ceb68b66c8d7aa5ad296857416a93
Lucas <lgrati@fi.uba.ar>
2024-08-30 00:26:04 -0300

    ejercicio 4

--- client/common/client.go
+++ client/common/client.go
@@ -4,6 +4,10 @@ import (
 	"bufio"
 	"fmt"
 	"net"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
 	"time"
 
 	"github.com/op/go-logging"
@@ -23,6 +27,8 @@ type ClientConfig struct {
 type Client struct {
 	config ClientConfig
 	conn   net.Conn
+	stop   chan struct{}
+	wg     sync.WaitGroup
 }
 
 // NewClient Initializes a new client receiving the configuration
@@ -30,6 +36,7 @@ type Client struct {
 func NewClient(config ClientConfig) *Client {
 	client := &Client{
 		config: config,
+		stop:   make(chan struct{}),
 	}
 	return client
 }
@@ -45,6 +52,7 @@ func (c *Client) createClientSocket() error {
 			c.config.ID,
 			err,
 		)
+		return err
 	}
 	c.conn = conn
 	return nil
@@ -52,38 +60,69 @@ func (c *Client) createClientSocket() error {
 
 // StartClientLoop Send messages to the client until some time threshold is met
 func (c *Client) StartClientLoop() {
-	// There is an autoincremental msgID to identify every message sent
-	// Messages if the message amount threshold has not been surpassed
+
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	c.wg.Add(1)
+	defer c.wg.Done()
+
 	for msgID := 1; msgID <= c.config.LoopAmount; msgID++ {
-		// Create the connection the server in every loop iteration. Send an
-		c.createClientSocket()
-
-		// TODO: Modify the send to avoid short-write
-		fmt.Fprintf(
-			c.conn,
-			"[CLIENT %v] Message N°%v\n",
-			c.config.ID,
-			msgID,
-		)
-		msg, err := bufio.NewReader(c.conn).ReadString('\n')
-		c.conn.Close()
-
-		if err != nil {
-			log.Errorf("action: receive_message | result: fail | client_id: %v | error: %v",
-				c.config.ID,
-				err,
-			)
+		select {
+		case <-c.stop:
+			log.Infof("action: loop_terminated | result: success | client_id: %v", c.config.ID)
 			return
+		case sig := <-sigs:
+			log.Infof("action: signal_received | signal: %v | client_id: %v", sig, c.config.ID)
+			c.StopClient()
+			return
+		default:
+			// Create the connection to the server in every loop iteration
+			err := c.createClientSocket()
+			if err != nil {
+				return
+			}
+
+			// Send the message
+			// TODO: Modify the send to avoid short-write
+			fmt.Fprintf(
+				c.conn,
+				"[CLIENT %v] Message N°%v\n",
+				c.config.ID,
+				msgID,
+			)
+
+			msg, err := bufio.NewReader(c.conn).ReadString('\n')
+			c.conn.Close()
+
+			if err != nil {
+				log.Errorf("action: receive_message | result: fail | client_id: %v | error: %v",
+					c.config.ID,
+					err,
+				)
+				return
+			}
+
+			log.Infof("action: receive_message | result: success | client_id: %v | msg: %v",
+				c.config.ID,
+				msg,
+			)
+
+			// Wait a time between sending one message and the next one
+			time.Sleep(c.config.LoopPeriod)
 		}
-
-		log.Infof("action: receive_message | result: success | client_id: %v | msg: %v",
-			c.config.ID,
-			msg,
-		)
-
-		// Wait a time between sending one message and the next one
-		time.Sleep(c.config.LoopPeriod)
-
 	}
 	log.Infof("action: loop_finished | result: success | client_id: %v", c.config.ID)
+}
+
+// Gracefully shut down the client
+func (c *Client) StopClient() {
+	close(c.stop)
+	c.wg.Wait()
+
+	if c.conn != nil {
+		c.conn.Close()
+		log.Infof("action: close_connection | result: success | client_id: %v", c.config.ID)
+	}
+	log.Infof("action: client_shutdown | result: success | client_id: %v", c.config.ID)
 }
--- server/common/server.py
+++ server/common/server.py
@@ -1,6 +1,8 @@
 import socket
 import logging
-
+import signal
+import threading
+import sys
 
 class Server:
     def __init__(self, port, listen_backlog):
@@ -8,21 +10,35 @@ class Server:
         self._server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._server_socket.bind(('', port))
         self._server_socket.listen(listen_backlog)
+        self._shutdown_flag = threading.Event()
+
+        # Signal handlers 
+        signal.signal(signal.SIGTERM, self.__handle_shutdown_signal)
+        signal.signal(signal.SIGINT, self.__handle_shutdown_signal)
+
+    def __handle_shutdown_signal(self, signum, frame):
+        signal_name = 'SIGTERM' if signum == signal.SIGTERM else 'SIGINT'
+        logging.info(f'action: receive_signal | signal: {signal_name} | result: in_progress')
+        self._shutdown_flag.set()  
+        self._server_socket.close()  
+        logging.info('action: shutdown_server | result: success')
+        sys.exit(0)  
 
     def run(self):
         """
         Dummy Server loop
 
         Server that accept a new connections and establishes a
-        communication with a client. After client with communucation
+        communication with a client. After client with communication
         finishes, servers starts to accept new connections again
         """
 
-        # TODO: Modify this program to handle signal to graceful shutdown
-        # the server
-        while True:
-            client_sock = self.__accept_new_connection()
-            self.__handle_client_connection(client_sock)
+        while not self._shutdown_flag.is_set():
+            try:
+                client_sock = self.__accept_new_connection()
+                self.__handle_client_connection(client_sock)
+            except OSError:
+                break
 
     def __handle_client_connection(self, client_sock):
         """
@@ -39,9 +55,10 @@ class Server:
             # TODO: Modify the send to avoid short-writes
             client_sock.send("{}\n".format(msg).encode('utf-8'))
         except OSError as e:
-            logging.error("action: receive_message | result: fail | error: {e}")
+            logging.error(f"action: receive_message | result: fail | error: {e}")
         finally:
             client_sock.close()
+            logging.info(f'action: close_client_socket | result: success | ip: {addr[0]}')
 
     def __accept_new_connection(self):
         """
