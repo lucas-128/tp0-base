26437f451e5b5d842128075f2f7a961cf8eed5d3
Lucas <lgrati@fi.uba.ar>
2024-09-03 16:36:05 -0300

    timeout check

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -2,10 +2,13 @@ NETWORK_NAME="tp0-base_testing_net"
 SERVICE_NAME="server"  
 PORT="12345"
 MESSAGE="Test"
+TIMEOUT_DURATION="5s" 
 
 docker run --rm --network "$NETWORK_NAME" busybox sh -c "
-  RESPONSE=\$(echo '$MESSAGE' | nc $SERVICE_NAME $PORT)
-  if [ \"\$RESPONSE\" = \"$MESSAGE\" ]; then
+  RESPONSE=\$(timeout $TIMEOUT_DURATION sh -c 'echo \"$MESSAGE\" | nc $SERVICE_NAME $PORT')
+  if [ -z \"\$RESPONSE\" ]; then
+    echo 'action: test_echo_server | result: fail'
+  elif [ \"\$RESPONSE\" = \"$MESSAGE\" ]; then
     echo 'action: test_echo_server | result: success'
   else
     echo 'action: test_echo_server | result: fail'
