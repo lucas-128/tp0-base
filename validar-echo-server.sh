f1b5117926bf2ce1d99c3a63264e362f5ea5f607
Lucas <lgrati@fi.uba.ar>
2024-09-03 20:31:56 -0300

    test

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,23 +1,8 @@
-NETWORK_NAME="tp0-base_testing_net"
+NETWORK_NAME="testing_net"
 SERVICE_NAME="server"
 PORT="12345"
 MESSAGE="Test"
 TIMEOUT_DURATION="5"  
-
-
-NETWORK_EXISTS=$(docker network ls --filter name="$NETWORK_NAME" --format '{{.Name}}')
-
-if [ "$NETWORK_EXISTS" != "$NETWORK_NAME" ]; then
-  echo 'action: test_echo_server | result: success'
-  exit 1
-fi
-
-SERVICE_REACHABLE=$(docker run --rm --network "$NETWORK_NAME" alpine sh -c "nc -z $SERVICE_NAME $PORT 2>/dev/null && echo 'reachable' || echo 'unreachable'")
-
-if [ "$SERVICE_REACHABLE" != "reachable" ]; then
-  echo 'action: test_echo_server | result: unreach'
-  exit 1
-fi
 
 docker run --rm --network "$NETWORK_NAME" alpine sh -c "
   RESPONSE=\$(echo '$MESSAGE' | nc -w $TIMEOUT_DURATION $SERVICE_NAME $PORT)
